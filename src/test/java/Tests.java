defd6754b70136b4c6e4ecf307b721ecf6293f32
ECRSSS <n.n.glebanov@gmail.com>
2019-06-10 00:11:54 +0300

    Задание 1.

---
+++ src/test/java/Tests.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tests {
+
+    private WebDriver driver;
+
+    @Before
+    public void setupWebDriver(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void test(){
+            driver = new ChromeDriver();
+            driver.get("https://otus.ru/");
+    }
+
+    @After
+    public void quit(){
+        if(driver!=null) {
+        driver.quit();
+        }
+    }
+}
